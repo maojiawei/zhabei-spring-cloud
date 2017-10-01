82e1fe34a99adaf39ba4df4c604e34bf7413bab7
maojiawei2012180@126.com <52175lqw>
2017-10-01 16:44:37 +0800

    config-client

---
+++ zhabei-config-client/src/main/java/io/jovi/zhabei/config/client/ConfigClientApplication.java
@@ -0,0 +1,29 @@
+package io.jovi.zhabei.config.client;/**
+ * Created by jovi on 01/10/2017.
+ */
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * <p>
+ * Title:
+ * </p>
+ * <p>
+ * Description:
+ * </p>
+ * <p>
+ * Copyright: Copyright (c) 2016
+ * All rights reserved. 2017-10-01 16:24
+ * </p>
+ *
+ * @author MaoJiaWei
+ * @version 1.0
+ */
+@SpringBootApplication
+public class ConfigClientApplication {
+    public static void main(String[] args) {
+
+        SpringApplication.run(ConfigClientApplication.class, args);
+    }
+}
---
+++ zhabei-config-client/src/main/java/io/jovi/zhabei/config/client/controller/ConfigClientController.java
@@ -0,0 +1,33 @@
+package io.jovi.zhabei.config.client.controller;/**
+ * Created by jovi on 01/10/2017.
+ */
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * <p>
+ * Title:
+ * </p>
+ * <p>
+ * Description:
+ * </p>
+ * <p>
+ * Copyright: Copyright (c) 2016
+ * All rights reserved. 2017-10-01 16:26
+ * </p>
+ *
+ * @author MaoJiaWei
+ * @version 1.0
+ */
+@RestController
+public class ConfigClientController {
+    @Value("${profile}")
+    private String profile;
+
+    @GetMapping("/profile")
+    public String hello() {
+        return this.profile;
+    }
+}
