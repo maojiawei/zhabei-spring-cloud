ec23d895bbdad3f667b1235017d21e05c566e372
maojiawei2012180@126.com <52175lqw>
2017-10-01 17:01:39 +0800

    spring cloud bus

--- zhabei-config-client/src/main/java/io/jovi/zhabei/config/client/controller/ConfigClientController.java
+++ zhabei-config-client/src/main/java/io/jovi/zhabei/config/client/controller/ConfigClientController.java
@@ -3,6 +3,7 @@ package io.jovi.zhabei.config.client.controller;/**
  */
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,6 +23,7 @@ import org.springframework.web.bind.annotation.RestController;
  * @version 1.0
  */
 @RestController
+@RefreshScope
 public class ConfigClientController {
     @Value("${profile}")
     private String profile;
